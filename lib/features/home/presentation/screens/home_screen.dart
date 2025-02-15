0089579528905926fa69964f00b2f4f57d1e9a8a
unknown <ayanasser1853@gmail.com>
2025-02-15 12:25:30 +0200

    add sapcing in column rather than sizedbox and app appBar rather cutom appBar

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:monitor_ship_project/core/utils/constants/app_assets.dart';
 import 'package:monitor_ship_project/core/utils/constants/colors.dart';
-import 'package:monitor_ship_project/features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/home_image.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/home_image_two.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/home_row_text.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/list_category_item.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/list_product_item.dart';
 import 'package:monitor_ship_project/features/home/presentation/widgets/recommended_item_list_view.dart';
+import 'package:monitor_ship_project/features/home/presentation/widgets/top_collection_row.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
@@ -15,31 +17,34 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
+      appBar: AppBar(
+        leading: Transform.scale(
+          scale: .35,
+          child: SvgPicture.asset(
+            AppAssets.icon,
+            height: 18,
+            width: 17,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.white,
+        title: Text('Discover'),
+        actions: [Container()],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 10,
             children: [
-              const CustomAppBar(text: 'Gemstore',),
+              //  const CustomAppBar(text: 'Gemstore',),
               const CategoryItemListView(),
-              const SizedBox(
-                height: 5,
-              ),
               const HomeImage(),
-              const SizedBox(
-                height: 10,
-              ),
               const CustomFeatureProductsRow(
                 text: 'Feature Products',
               ),
-              const SizedBox(
-                height: 10,
-              ),
               const ProductItemListView(),
-              const SizedBox(
-                height: 10,
-              ),
               const HomeImage2(
                 text1: 'NEW COLLECTION',
                 text2: 'HANG OUT',
@@ -49,61 +54,28 @@ class Homescreen extends StatelessWidget {
               const CustomFeatureProductsRow(
                 text: 'Recommended',
               ),
-              const SizedBox(
-                height: 10,
-              ),
+             
               const RecommendedItemListView(),
-              const SizedBox(
-                height: 10,
-              ),
+             
               const CustomFeatureProductsRow(
                 text: 'Top Collection',
               ),
-              const SizedBox(
-                height: 10,
-              ),
+             
               const HomeImage2(
                 text1: 'Sale up to 40%',
                 text2: 'FOR SLIM ',
                 image: 'assets/images/image 48.png',
                 text3: '& BEAUTY',
               ),
-              const SizedBox(
-                height: 15,
-              ),
+           
               const HomeImage2(
                 text1: 'Summer Collection 2021',
                 text2: 'Most sexy  ',
                 image: 'assets/images/image 69.png',
                 text3: '&design',
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 200,
-                      decoration: BoxDecoration(
-                        
-                        borderRadius: BorderRadius.circular(12), ),
-                      child: Image.asset('assets/images/banner 3.png'),
-                    ),
-                  ),
-                    Expanded(
-                      child: Container(
-                                      height: 200,
-                                     // width: 200,
-                                      decoration: BoxDecoration(
-                                        
-                                        borderRadius: BorderRadius.circular(12), ),
-                                      child: Image.asset('assets/images/banner 4.png'),
-                                    ),
-                    ),
-                ],
-              ),
-             
+            
+              TopCollectionRow(),
             ],
           ),
         ),
@@ -111,3 +83,4 @@ class Homescreen extends StatelessWidget {
     );
   }
 }
+
