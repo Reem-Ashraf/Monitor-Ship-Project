bc194a95fc739772e61805cca68557e93f00f8e4
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2025-03-12 22:16:04 +0200

    Add Firebase integration and configuration for Android and iOS platforms

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,15 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/bloc_observer.dart';
+import 'firebase_options.dart';
 import 'my_app.dart';
 
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = MyBlocObserver();
   await ScreenUtil.ensureScreenSize();
-  WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(EasyLocalization(
       supportedLocales: [
@@ -17,7 +22,7 @@ void main() async{
         Locale('en'),
       ],
       path: 'assets/translations',
-      startLocale: const Locale('en'), // to test the app in English, change this to Locale('en')
+      startLocale: const Locale('en'),
       child: const MyApp()));
 }
 
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,14 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:monitor_ship_project/firebase_options.dart';
 import 'core/bloc_observer.dart';
 import 'my_app.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = MyBlocObserver();
   await ScreenUtil.ensureScreenSize();
-  WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   runApp(EasyLocalization(
       supportedLocales: [
